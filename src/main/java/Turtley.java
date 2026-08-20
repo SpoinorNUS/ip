26e5f62a52e8e62cb4771c8b6a12dd9785bec228
Spoinor <e1525440@u.nus.edu>
2026-08-20 11:24:21 +0800

    Hand-wrote code for application to echo commands by user and terminate at bye command

--- src/main/java/Turtley.java
+++ src/main/java/Turtley.java
@@ -1,21 +1,69 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Turtley {
+
     private static final String SEPARATOR = "____________________________________________________________";
+
+    public static void list() {
+        System.out.println(SEPARATOR);
+        System.out.println("list");
+        System.out.println(SEPARATOR);
+    }
+
+    public static void blah() {
+        System.out.println(SEPARATOR);
+        System.out.println("blah");
+        System.out.println(SEPARATOR);
+    }
+
+    public static void bye() {
+        System.out.println(SEPARATOR);
+        System.out.println("Bye. See you around!");
+        System.out.println(SEPARATOR);
+    }
+
+    public static boolean prompt(Scanner keyboard) {
+        String input = keyboard.nextLine();
+        if (Objects.equals(input,"list")) {
+            list();
+            return true;
+        } else if (Objects.equals(input, "blah")) {
+            blah();
+            return true;
+        } else if (Objects.equals(input, "bye")) {
+            bye();
+            return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         //Turtley ASCII art was by me.
-        String banner = "      _____________ \n"
-                + "__   /__|_______|__\\ \n"
-                + "\\^ \\/______|_|______\\\n"
-                + " \\ /_______|_|_______\\>\n"
-                + "   |_/ |_/     \\_| \\_|\n Turtley";
-        
+        String banner = """
+                      _____________\s
+                __   /__|_______|__\\\s
+                \\^ \\/______|_|______\\
+                 \\ /_______|_|_______\\>
+                   |_/ |_/     \\_| \\_|
+                 Turtley""";
+
         //Line 11-17 was written by ChatGPT.
         System.out.println(SEPARATOR);
         System.out.println(banner);
         System.out.println("Hello! I'm Turtley.");
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
-        System.out.println("Bye. See you around!");
-        System.out.println(SEPARATOR);
+
+        //running variable is true when the application is running
+        boolean running = true;
+
+        //Initialise Scanner of user inputs
+        Scanner keyboard = new Scanner(System.in);
+
+        //Keep prompting the user while app is running
+        while (running) {
+            running = prompt(keyboard);
+        }
     }
 }
