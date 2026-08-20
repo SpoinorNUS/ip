e91d6512ad782d16df397da4be3441b59aadc334
Spoinor <e1525440@u.nus.edu>
2026-08-20 10:36:23 +0800

    Updated intial code to greet and user and immediately exit

--- src/main/java/Turtley.java
+++ src/main/java/Turtley.java
@@ -1,10 +1,21 @@
 public class Turtley {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
+        //Turtley ASCII art was by me.
         String banner = "      _____________ \n"
                 + "__   /__|_______|__\\ \n"
                 + "\\^ \\/______|_|______\\\n"
                 + " \\ /_______|_|_______\\>\n"
-                + "   |_/ |_/     \\_| \\_|\n TURTLEY";
+                + "   |_/ |_/     \\_| \\_|\n Turtley";
+        
+        //Line 11-17 was written by ChatGPT.
+        System.out.println(SEPARATOR);
         System.out.println(banner);
+        System.out.println("Hello! I'm Turtley.");
+        System.out.println("What can I do for you?");
+        System.out.println(SEPARATOR);
+        System.out.println("Bye. See you around!");
+        System.out.println(SEPARATOR);
     }
 }
