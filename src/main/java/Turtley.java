b3610759a993a9848b84e45a3302a768cf43963c
Spoinor <e1525440@u.nus.edu>
2026-08-20 19:32:39 +0800

    Used Codex to allow tasks to be marked as done using mark(). Updated prompt() to include mark()

--- src/main/java/Turtley.java
+++ src/main/java/Turtley.java
@@ -5,6 +5,7 @@ public class Turtley {
     private static final String SEPARATOR = "____________________________________________________________";
     private static final int MAX_TASK_NUM = 100;
     private static final String[] taskList = new String[MAX_TASK_NUM];
+    private static final boolean[] completedTasks = new boolean[MAX_TASK_NUM];
     private static int numOfTasks = 0;
 
     //When user inputs any non-commands, add the exact words as a task to task list
@@ -16,6 +17,7 @@ public class Turtley {
             return;
         }
         taskList[numOfTasks] = input;
+        completedTasks[numOfTasks] = false;
         numOfTasks++;
         System.out.println(SEPARATOR);
         System.out.println("added: " + input);
@@ -33,13 +35,38 @@ public class Turtley {
 
         //Print out list display
         System.out.println(SEPARATOR);
+        System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < taskList.length; i++) {
             if (i >= numOfTasks) {
                 break;
             }
-            System.out.println((i+1)+". " + taskList[i]);
+            String status = completedTasks[i] ? "[X]" : "[ ]";
+            System.out.println(" " + (i + 1) + "." + status + " " + taskList[i]);
         }
         System.out.println(SEPARATOR);
+    }
+
+    //Marks the task at the given one-based list index as done. (Written by ChatGPT)
+    public static void mark(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input) - 1;
+            if (taskIndex < 0 || taskIndex >= numOfTasks) {
+                System.out.println(SEPARATOR);
+                System.out.println(" Task number is not in your list.");
+                System.out.println(SEPARATOR);
+                return;
+            }
+
+            completedTasks[taskIndex] = true;
+            System.out.println(SEPARATOR);
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   [X] " + taskList[taskIndex]);
+            System.out.println(SEPARATOR);
+        } catch (NumberFormatException exception) {
+            System.out.println(SEPARATOR);
+            System.out.println(" Please provide a valid task number.");
+            System.out.println(SEPARATOR);
+        }
     }
 
     //Terminates the app
@@ -60,7 +87,14 @@ public class Turtley {
             case "" -> {System.out.println("Please input something."); return true;}
             case "bye" -> {bye(); return false;}
             case "list" -> {list(); return true;}
-            default -> {add(input); return true;}
+            default -> {
+                if (input.startsWith("mark ")) { //(Written by ChatGPT)
+                    mark(input.substring(5).trim());
+                } else {
+                    add(input);
+                }
+                return true;
+            }
         }
     }
 
